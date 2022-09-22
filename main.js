d72d5d711f98ab1d0fff49bf30a6cd227bed2a33
Wataru Hori <Bireleysaddiction@gmail.com>
2022-09-22 18:50:12 +0900

    Updated: User's github icon will appear when we get the data

--- main.js
+++ main.js
@@ -1,54 +1,56 @@
-const submitButton = document.getElementById('submit')
-const userInput = document.getElementById('username')
-const placeholder = document.querySelector('.userinfo')
-const API_URL = 'https://api.github.com/users/'
+const submitButton = document.getElementById('submit');
+const userInput = document.getElementById('username');
+const placeholder = document.querySelector('.userinfo');
+const API_URL = 'https://api.github.com/users/';
 
 
 function getApi() {
-  placeholder.textContent = ''
+  placeholder.textContent = '';
   fetch(API_URL + encodeURIComponent(userInput.value))
       .then(res => res.json())
       .then(data => {
-        const previousData = document.querySelectorAll('.added')
+        const previousData = document.querySelectorAll('.added');
 
         if (previousData) {
-          previousData.forEach(e => e.remove())
-        }
-        const userName = document.createElement('p')
-        userName.textContent = `Username: ${data.login}`
+          previousData.forEach(e => e.remove());
+        };
         
-        console.log(userName)
-        const userBio = document.createElement('p')
+        const userName = document.createElement('p');
+        userName.textContent = `Username: ${data.login}`;
+        
+        const userBio = document.createElement('p');
         
         if (!data.login)
-         return placeholder.textContent = 'We could not find the user, Sorry!'
+         return placeholder.textContent = 'We could not find the user, Sorry!';
         
-
         if (!data.bio) {
-          userBio.textContent = 'This user has no bio yet.'
+          userBio.textContent = 'This user has no bio yet.';
         } else {
           userBio.textContent = data.bio;
-        }
+        };
 
-        const userURL = document.createElement('p')
+        const userIcon = document.createElement('img');
+        userIcon.setAttribute('src', `${data.avatar_url}`);
+        userIcon.style.display = 'block';
+
+        const userURL = document.createElement('p');
         userURL.textContent = `URL: ${data.html_url}`;
 
-        userName.className = 'added'
-        userBio.className = 'added'
-        userURL.className = 'added'
+        userName.className = 'added';
+        userBio.className = 'added';
+        userURL.className = 'added';
+        userIcon.className = 'added usericon';
 
+        const image = document.querySelector('.image-placeholder');
+
+        image.appendChild(userIcon);
+        
         placeholder.appendChild(userName);
         placeholder.appendChild(userBio);
         placeholder.appendChild(userURL);
+
       })
       .catch(err => console.log(err));
-}
+};
 
-
-
-submitButton.addEventListener('click', getApi)
-
-
-
-
-
+submitButton.addEventListener('click', getApi);
